12a5c7ad5dbe048d3767827091d08b0ba27519b9
Daniel Krastev <danielkrastev01@gmail.com>
2025-02-14 21:31:24 +0200

    Created crypto model

---
+++ src/models/crypto-model.js
@@ -0,0 +1,47 @@
+import { Schema, model, Types } from "mongoose";
+
+const cryptoSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 10,
+    },
+    image: {
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 20,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+        enum: [
+            'crypto-wallet',
+            'credit-card',
+            'debit-card',
+            'paypal',
+        ],
+    },
+    buyCrypto: [
+        {
+            type: Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    owner: {
+        type: Types.ObjectId,
+        ref: 'User',
+    },
+});
+
+const Crypto = model('Crypto', cryptoSchema);
+
+export default Crypto;
