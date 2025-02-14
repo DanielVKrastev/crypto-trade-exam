c6f15c66a1fd4a164602463001f51b711b2ba5f6
Daniel Krastev <danielkrastev01@gmail.com>
2025-02-14 22:32:09 +0200

    Add delete function

--- src/middlewares/owner-middleware.js
+++ src/middlewares/owner-middleware.js
@@ -1,12 +1,11 @@
 import cryptoService from "../services/crypto-service.js";
 
-
 export const isOwner = async (req, res, next) => {
-    const crypto = await cryptoService.getOne(req.params.volcanoId);
+    const crypto = await cryptoService.getOne(req.params.cryptoId);
 
     if(! crypto.owner.equals(req.user.id)){
         res.setError('You are not owner of this offer');
-        return res.redirect(`/volcanoes/${crypto.id}/details`);
+        return res.redirect(`/crypto/${crypto.id}/details`);
     }
 
     req.crypto = crypto;
